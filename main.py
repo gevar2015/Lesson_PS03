29d4616dd72de486313e0ea677c9c40b05725aec
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-06-20 19:45:29 +0300

    add translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from googletrans import Translator
 
 # Создаём функцию, которая будет получать информацию
 def get_english_words():
@@ -10,18 +11,24 @@ def get_english_words():
         # Создаём объект Soup
         soup = BeautifulSoup(response.content, "html.parser")
         # Получаем слово. text.strip удаляет все пробелы из результата
-        english_words = soup.find("div", id="random_word").text.strip()
+        english_word = soup.find("div", id="random_word").text.strip()
         # Получаем описание слова
         word_definition = soup.find("div", id="random_word_definition").text.strip()
         # Чтобы программа возвращала словарь
         return {
-            "english_words": english_words,
+            "english_word": english_word,
             "word_definition": word_definition
         }
     # Функция, которая сообщит об ошибке, но не остановит программу
     except Exception as e:
         print(f"Произошла ошибка: {e}")
         return None
+
+# Функция перевода слов и описаний
+def translate_to_russian(text):
+    translator = Translator()
+    translation = translator.translate(text, dest='ru')
+    return translation.text
 
 # Создаём функцию, которая будет делать саму игру
 def word_game():
@@ -33,16 +40,20 @@ def word_game():
             print("Не удалось получить слово. Попробуйте снова.")
             continue
 
-        word = word_dict.get("english_words")
+        english_word = word_dict.get("english_word")
         word_definition = word_dict.get("word_definition")
 
+        # Переводим слово и описание на русский
+        russian_word = translate_to_russian(english_word)
+        russian_definition = translate_to_russian(word_definition)
+
         # Начинаем игру
-        print(f"Значение слова - {word_definition}")
+        print(f"Значение слова - {russian_definition}")
         user = input("Что это за слово? ")
-        if user == word:
+        if user.lower() == russian_word.lower():
             print("Все верно!")
         else:
-            print(f"Ответ неверный, было загадано это слово - {word}")
+            print(f"Ответ неверный, было загадано это слово - {russian_word}")
 
         # Создаём возможность закончить игру
         play_again = input("Хотите сыграть еще раз? y/n: ")
